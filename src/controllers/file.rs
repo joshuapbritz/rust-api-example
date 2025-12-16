0a36397fdc8ce27b1d87b42dbbb920325c7eb169
Joshua Britz <joshua@luckybeard.com>
2025-12-16 19:11:45 +0200

    feat(endpoints): added json upload

--- src/controllers/file.rs
+++ src/controllers/file.rs
@@ -31,13 +31,26 @@ pub async fn post_upload_csv(
 }
 
 pub async fn post_upload_json(
-    _user_id: Uuid,
+    user_id: Uuid,
     data: warp::multipart::FormData,
 ) -> Result<impl warp::Reply, warp::Rejection> {
+    let mut connection = database::get().map_err(|error| ServiceError::from(error))?;
+
     let todos = parser::process_json(data).await?;
 
+    let inserted_todos =
+        InsertableTodo::insert_many(todos, &mut connection).map_err(ServiceError::from)?;
+
+    let user_todo_links: Vec<UserTodo> = inserted_todos
+        .iter()
+        .map(|todo| UserTodo::new(todo.id, user_id))
+        .collect();
+
+    let linked =
+        UserTodo::link_many(user_todo_links, &mut connection).map_err(ServiceError::from)?;
+
     Ok(warp::reply::with_status(
-        warp::reply::json(&todos),
+        warp::reply::json(&serde_json::json!({ "total": linked.len() })),
         warp::http::StatusCode::OK,
     ))
 }
